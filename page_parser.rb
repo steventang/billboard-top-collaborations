b647d169a2061837d2189d985a46b39ad6c15a36
Steven Tang <stang12290@gmail.com>
2015-08-20 11:38:06 -0700

    finish parsing

--- page_parser.rb
+++ page_parser.rb
@@ -1,28 +1,26 @@
-require 'mechanize'
 require 'nokogiri'
 require 'open-uri'
-
-#Song = Struct.new(:title, :date, :artist)#
-
-# agent = Mechanize.new { |agent|
-	# agent.user_agent_alias = 'Windows Chrome'
-# }
-# 
-# page = agent.get('http://www.billboard.com/charts/hot-100/1990-02-03')
-# 
-# puts page.at('span.this-week').text
-# puts page.at('div.row-title h2').text
-# puts page.at('div.row-title h3').text
+require 'csv'
 
 date = Date.new(1990, 1, 6)
-end_date = Date.new(1990, 1, 27)
+end_date = Date.new(2015, 8, 22)
+entries_per_week = 10
 
-while date < end_date
-	page = Nokogiri::HTML(open("http://www.billboard.com/charts/hot-100/#{date.to_s}"))# 
-	song_ranks = page.css('span.this-week')
-	song_names = page.css('div.row-title h2')
-	song_artists = page.css("div.row-title h3") #.css("a[data-tracklabel='Artist Name']")# 
-	puts song_ranks[5].text
-	puts song_names[5].text
-	puts song_artists[1].text
+CSV.open('data/song_data.csv', "w") do |file|
+	file << %w(date rank title artist)
+	while date <= end_date
+		page = Nokogiri::HTML(open("http://www.billboard.com/charts/hot-100/#{date.to_s}"))
+		song_ranks = page.css('span.this-week')
+		song_names = page.css('div.row-title h2')
+		song_artists = page.css("div.row-title h3")
+		for i in 0..entries_per_week-1
+			file << [date.to_s,
+							 song_ranks[i].text.strip,
+							 song_names[i].text.strip,
+							 song_artists[i].text.strip
+							]
+		end
+		date += 7
+		sleep 0.3
+	end
 end
